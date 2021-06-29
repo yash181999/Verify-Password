5acef3792e554c6f52b6503cdca4b7e1937c6fa6
yashmehta <yash181999@gmail.com>
2021-06-29 07:15:11 +0530

    Password Verfication

---
+++ password.js
@@ -0,0 +1,19 @@
+
+const checkPassword = (password) =>{
+     if(password === undefined || password === null || password == '' || password.length < 8) {
+         throw "Password must be of atleast 8 characters";
+     }   
+     else if(!/[A-Z]/.test(password)) {
+         throw "Password must contain a captial letter";
+     } 
+     else if(!/[a-z]/.test(password)) {
+         throw "Password must contain a small letter";
+     }
+     else if(!/[0-9]/.test(password)) {
+         throw "Password must contain a number";
+     }
+     else{
+         return "Correct Password";
+     }
+}
+
